afb6bbd381f59f3c46b3efeddde9ad8cd7e7ed36
nhanngfast <nhannguyen6533@gmail.com>
2025-12-17 15:15:50 +0700

    feat: completed the scrolling text

--- chat-chiu-web/src/pages/Home/Home.jsx
+++ chat-chiu-web/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HeroBanner from "../../components/hero-banner/HeroBanner";
+import ScrollingText from "../../components/scrolling-text/ScrollingText";
 import "./Home.css";
 
 const Home = () => {
   return <div className="home-container">
     <HeroBanner />
+    <ScrollingText />
   </div>;
 };
 
