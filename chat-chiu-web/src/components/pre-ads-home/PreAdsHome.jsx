124d6d09965d62a9c101edcce3ff3168fb951a9d
nhanngfast <nhannguyen6533@gmail.com>
2026-02-04 21:16:43 +0700

    feat: added pre ads section of homepage

---
+++ chat-chiu-web/src/components/pre-ads-home/PreAdsHome.jsx
@@ -0,0 +1,16 @@
+import "./PreAdsHome.css";
+import EastIcon from '@mui/icons-material/East';
+
+const PreAdsHome = () => {
+  return (
+    <section className="pre-ads-home-container">
+      <h1>Nuôi dưỡng làn da khỏe đẹp cùng Chắt Chiu</h1>
+      <button className="pre-ads-home-btn">
+        <p>Tìm hiểu thêm</p>
+        <EastIcon />
+      </button>
+    </section>
+  );
+};
+
+export default PreAdsHome;
--- chat-chiu-web/src/pages/Home/Home.jsx
+++ chat-chiu-web/src/pages/Home/Home.jsx
@@ -2,15 +2,19 @@ import HeroBanner from "../../components/hero-banner/HeroBanner";
 import InternationalCertificates from "../../components/international-certificates/InternationalCertificates";
 import ScrollingText from "../../components/scrolling-text/ScrollingText";
 import SubBanner from "../../components/sub-banner/SubBanner";
+import PreAdsHome from "../../components/pre-ads-home/PreAdsHome";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home-container">
-    <HeroBanner />
-    <ScrollingText />
-    <SubBanner />
-    <InternationalCertificates />
-  </div>;
+  return (
+    <div className="home-container">
+      <HeroBanner />
+      <ScrollingText />
+      <SubBanner />
+      <InternationalCertificates />
+      <PreAdsHome />
+    </div>
+  );
 };
 
 export default Home;
